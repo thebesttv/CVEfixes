590c29e67c17cedf9d1e5a19cb744f7efcfe2843
thebesttv <taojuntian@outlook.com>
2023-10-23 20:52:00 +0800

    Use more general expr for grep

--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,4 @@
-tail -n +1 -f log | stdbuf -o0 grep 'repo .* of 3849' | while read -r line; do
+tail -n +1 -f log | stdbuf -o0 grep 'Retrieving fixes for repo .* of .* - ' | while read -r line; do
   echo "$line"
   curl -s -d "$line" "ntfy.sh/${1}-3bRpJfPnt8hEP9wB" >/dev/null
 done
-
